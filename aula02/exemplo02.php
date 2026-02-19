1fd1c4dad5bf768c31110389063588de7c3803d1
pietrorodrigues09 <pietrorodriguesetec2025@gmail.com>
2026-02-19 08:48:38 -0300

    aula 02

---
+++ aula02/exemplo02.php
@@ -0,0 +1,13 @@
+<?php
+    //VERIFICAR CONTEÚDOS E TIPOS DE UMA VARIÁVEL
+    $x = 10.5;
+    echo "O valor de x é: $x <br>";
+    echo "O valor de x é: ".$x;//CONCATENÇÃO
+    echo "<hr>";
+
+    //COMANDO DE DEPURAÇÃO
+    var_dump($x);
+    print_r($x);
+
+    
+?>
---
+++ aula02/exemplo04.php
@@ -0,0 +1,9 @@
+<?php
+    $idade = false;
+    if($idade){
+        echo"Maior de idade";
+    } else{
+        echo"Menor de idade";
+    }
+
+?>
