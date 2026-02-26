1d1a3ffb0a5f6bd177b6a0e6dbbd265ed819148c
pietrorodrigues09 <pietrorodriguesetec2025@gmail.com>
2026-02-26 08:53:42 -0300

    Add files via upload

---
+++ aula.03/exemplo01.php
@@ -0,0 +1,6 @@
+<?php 
+    $a = 4;
+    $b = "101 Dálmatas";
+    $s = $a . $b;
+    echo $s
+?>
---
+++ aula.03/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AULA 03</title>
+</head>
+<body>
+    <h1>Aula 03 - SW-I</h1>
+    <?php
+        $nome = "Pietro";
+        echo "<p style='color:red ;' > Bem-vindo, $nome </p>"; 
+    ?>
+    <p> Nome: <?php echo $nome ?></p>
+    
+    
+    
+    
+    
+    
+
+
+    
+</body>
+</html>
