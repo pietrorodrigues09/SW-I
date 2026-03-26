fe71d9df405b2a863fc6d4bc45665c3deaab6e58
pietrorodrigues09 <pietrorodriguesetec2025@gmail.com>
2026-03-26 12:20:53 -0300

    aula dia 26/03

---
+++ aula06/processa.php
@@ -0,0 +1,21 @@
+<?php 
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    //email: adm@gmail.com
+    //senha: 1234
+    
+
+    if ($email == 'adm@gmail.com' && $senha == '1234') {
+        //vamos para a área restrita
+        //echo 'vamos para a área restrita';
+        $nome = "PietoRodrigues";
+
+        header('Location: restrita.php?nome='.$nome);
+    } else {
+        //volta para o forms
+        //echo 'volta para o forms';
+        header('Location: formulário.php');
+    }
+    
+?>
---
+++ aula06/restrita.php
@@ -0,0 +1,14 @@
+<?php
+    $nome = $_GET['nome']
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AREA RESTRITA</title>
+</head>
+<body>
+    <p>Bem vindo <?php echo $nome; ?> a sua área restrita</p>
+</body>
+</html>
