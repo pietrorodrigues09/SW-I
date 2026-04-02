9372babcf6e1e409097de94709db0e646828bd87
pietrorodrigues09 <pietrorodriguesetec2025@gmail.com>
2026-04-02 12:26:26 -0300

    atualização 02/04

---
+++ atividadeparanota/bootstrap/erro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylerro.css">
+    <title>Error</title>
+</head>
+<body>
+    <div class="custom-bg text-dark">
+    <div class="d-flex align-items-center justify-content-center min-vh-100 px-2">
+        <div class="text-center">
+            <h1 class="display-1 fw-bold">404</h1>
+            <p class="fs-2 fw-medium mt-4">Seu login deu errado</p>
+            <p class="mt-4 mb-5">refaça o login</p>
+            <a href="index.php" class="btn btn-light fw-semibold rounded-pill px-4 py-2 custom-btn">
+                Voltar
+            </a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
