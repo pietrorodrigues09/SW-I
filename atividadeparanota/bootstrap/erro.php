d0eb68a60f0868cd740a568e34e0c80990058024
pietrorodrigues09 <pietrorodriguesetec2025@gmail.com>
2026-04-09 11:18:06 -0300

    página de erro 09/04

--- atividadeparanota/bootstrap/erro.php
+++ atividadeparanota/bootstrap/erro.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="stylerro.css">
+
     <title>Error</title>
 </head>
 <body>
-    <div class="custom-bg text-dark">
+<div class="custom-bg text-dark">
     <div class="d-flex align-items-center justify-content-center min-vh-100 px-2">
         <div class="text-center">
             <h1 class="display-1 fw-bold">404</h1>
             <p class="fs-2 fw-medium mt-4">Seu login deu errado</p>
-            <p class="mt-4 mb-5">refaça o login</p>
+            <p class="mt-4 mb-5">Refaça o login</p>
             <a href="index.php" class="btn btn-light fw-semibold rounded-pill px-4 py-2 custom-btn">
                 Voltar
             </a>
